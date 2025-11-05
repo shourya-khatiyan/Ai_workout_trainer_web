798d03fc0e33b44293034c25a89cd0d35e98bea7
Shourya Khatiyan <shauryakhatiyan@gmail.com>
2025-11-05 19:05:19 +0530

    directory cleaning

--- src/pages/exceptional.py
+++
@@ -1,9 +0,0 @@
-
-for i in range(99):
-    try:
-        n=int(input("number:"))
-        divide = 50/n
-        print("50 divided by", n, "is", divide)
-
-    except ZeroDivisionError:
-        print("0 value error")
